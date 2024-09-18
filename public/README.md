d246408342904612bc665f79b7cf704e5ecc1230
elfdrkn <drkn.elif@gmail.com>
2024-09-18 20:18:29 +0300

    commit

---
+++ public/README.md
@@ -0,0 +1,1 @@
+For this assignment, I recreated a sample page using Tailwind CSS. Many of Tailwind’s features were utilized in the design, showcasing its versatility and power.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+
+    extend: {
+      fontFamily: {  //Font familymi tanımladım
+        roboto: ['Roboto', 'sans-serif'],
+      },
+
+   
+    },
+  },
+  plugins: [],
+}
+
